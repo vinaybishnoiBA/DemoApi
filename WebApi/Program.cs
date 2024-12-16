6ac051029c32f8b450777a6d8195d13630c8cac2
Office PC <sumedha3006arora@gmail.com>
2024-12-16 10:16:59 +0530

    initial commit

---
+++ WebApi/Program.cs
@@ -0,0 +1,41 @@
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.UseCors("MyPolicy");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
